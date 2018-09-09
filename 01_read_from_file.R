601507dc43a487254de29ef624fd9ac350a30cf8
Ian Cook <icook@cloudera.com>
2018-09-09 18:49:08 +0000

    Add read data from file problems

---
+++ 01_read_from_file.R
@@ -0,0 +1,39 @@
+# Copyright 2018 Cloudera, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# # Read data from a delimited text file into an R data frame
+
+# Read data using the `read.table()` function
+# ...
+
+# See help for the `read.table()` function
+# ...
+
+# Read data using the `read.csv()` function
+# ...
+
+# View the data
+# ...
+
+# Load the readr package
+# ...
+
+# Read data using the `read_csv()` function
+# ...
+
+# View the data as a tibble
+# ...
+
+# View the data as a data frame
+# ...
---
+++ 01_read_from_file.py
@@ -0,0 +1,36 @@
+# Copyright 2018 Cloudera, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# # Read data from a delimited text file into a pandas DataFrame
+
+# Load modules
+# ...
+
+# Read data using the `read_table` function
+# ...
+
+# See help for the `read_table` function
+# ...
+
+# Read data using the `read_csv` function
+# ...
+
+# View the data
+# ...
+
+# To display the pandas DataFrame in a scrollable
+# grid, set the pandas option
+# `display.html.table_schema` to `True`:
+
+pd.set_option("display.html.table_schema", True)
