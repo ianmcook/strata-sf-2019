a0b109bd6455586f8108c9eab68d88e4b6cafabd
Ian Cook <icook@cloudera.com>
2018-09-11 07:09:36 +0000

    Add ordering rows examples

---
+++ 08_arrange.R
@@ -0,0 +1,41 @@
+# Copyright 2018 Cloudera, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# # Ordering rows in an R data frame
+
+# Load packages and read games data
+library(readr)
+library(dplyr)
+games <- read_csv("data/games/games.csv")
+games
+
+
+# Use the dplyr verb `arrange` to sort the rows of a data 
+# frame
+games %>% arrange(min_age)
+
+# The default sort order is ascending. Use the helper 
+# function `desc` to sort in descending order
+games %>% arrange(desc(min_age))
+
+# You can specify multiple columns to sort by
+games %>% arrange(desc(max_players), min_age)
+
+# After ordering rows, use the `head` function to limit
+# the number of rows returned, to get the "top N" results
+# For example: What are the two least expensive games?
+games %>% 
+  select(name, list_price) %>%
+  arrange(list_price) %>%
+  head(2)
---
+++ 08_sort_values.py
@@ -0,0 +1,45 @@
+# Copyright 2018 Cloudera, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# # Ordering rows in a pandas DataFrame
+
+# Import modules and read games data
+import numpy as np
+import pandas as pd
+games = pd.read_table('data/games/games.csv', sep=',')
+games
+
+# Use the DataFrame method `sort_values` to sort the rows of
+# a DataFrame
+games.sort_values('min_age')
+
+# The default sort order is ascending. Use the the parameter
+# `ascending` to control the sort order
+games.sort_values('min_age', ascending=False)
+
+# You can specify multiple columns to sort by, in a list
+# with the sort orders also specified in a list
+games \
+  .sort_values(
+    ['max_players','min_age'],
+    ascending=[False,True]
+  )
+
+# After ordering rows, use the `head` method to limit
+# the number of rows returned, to get the "top N" results
+# For example: What are the two least expensive games?
+games \
+  .filter(['name', 'list_price']) \
+  .sort_values('list_price') \
+  .head(2)
